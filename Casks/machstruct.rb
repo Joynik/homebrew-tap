3ceaedb0edbc8bbff25403fe76b0595a06d4bf5a
Joynik <joynik@gmail.com>
2026-05-18 20:44:06 +0300

    fix: correct URL format for MachStruct download

--- Casks/machstruct.rb
+++ Casks/machstruct.rb
@@ -2,7 +2,7 @@ cask "machstruct" do
   version "1.0.4"
   sha256 "91dedd4f20566bdfdc591821bc274960451f4b39a8551f08558ef9bda239e7e8"
 
-  url "https://github.com/lustech/MachStruct/releases/download/v#{version}/MachStruct-#{version}.dmg"
+  url "https://github.com/lustech/MachStruct/releases/download/v#{version}/MachStruct-v#{version}.dmg"
   name "MachStruct"
   desc "Native macOS viewer and editor for JSON, XML, YAML, CSV"
   homepage "https://github.com/lustech/MachStruct"
