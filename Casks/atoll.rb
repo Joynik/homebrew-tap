74d29d7166c495ab8e8026ee72185a61bf4de197
Joynik <joynik@gmail.com>
2026-05-22 13:33:39 +0300

    feat: add 'atoll' cask for macOS dynamic island application

---
+++ Casks/atoll.rb
@@ -0,0 +1,16 @@
+cask "atoll" do
+  version "2.1.0"
+  sha256 "add3a30d4e9bfa5cd180f2538c88d4cda047813dafe574fab5d0660087c3392b"
+
+  url "https://github.com/Ebullioscopic/Atoll/releases/download/v#{version}/Atoll.#{version}.dmg"
+  name "Atoll"
+  desc "Dynamic Island for macOS"
+  homepage "https://github.com/Ebullioscopic/Atoll"
+
+  livecheck do
+    url "https://github.com/Ebullioscopic/Atoll/releases/latest"
+    strategy :header_match
+  end
+
+  app "Atoll.app"
+end
