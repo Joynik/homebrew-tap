02cf70b58905d84d380f0565d68601e49e105ddc
Joynik <joynik@gmail.com>
2026-06-02 12:27:29 +0300

    feat: update 'atoll' cask to version 2.2.0 with updated description

--- Casks/atoll.rb
+++ Casks/atoll.rb
@@ -1,11 +1,13 @@
 cask "atoll" do
-  version "2.1.0"
-  sha256 "add3a30d4e9bfa5cd180f2538c88d4cda047813dafe574fab5d0660087c3392b"
+  version "2.2.0"
+  sha256 "30e532e69650a7183769dff222bca7106de7dfe4936e9bd9c2096e20f58a4cb2"
 
   url "https://github.com/Ebullioscopic/Atoll/releases/download/v#{version}/Atoll.#{version}.dmg"
   name "Atoll"
-  desc "Dynamic Island for macOS"
+  desc "Dynamic Island"
   homepage "https://github.com/Ebullioscopic/Atoll"
+
+  depends_on :macos
 
   livecheck do
     url :url
